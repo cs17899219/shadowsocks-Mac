460f4f2f5345aa8fe03f0cbd9798d400cb081550
C.S <cs17899219@gmail.com>
2016-06-24 09:33:38 +0800

    fix money malloc for profile_t

--- ShadowsocksX-NG/SSLocalManager.c
+++ ShadowsocksX-NG/SSLocalManager.c
@@ -17,11 +17,9 @@
 
 static pthread_t ss_thread;
 
-static profile_t my_profile;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static int isrunning = 0;
-
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /* this function is run by the second thread */
 void *do_ss_start(void* profile)
@@ -33,81 +31,27 @@ void *do_ss_start(void* profile)
     sprintf(str,"SSLocal %d",++i);
     pthread_setname_np(str);
     
-    profile_t profile1 = {
-          .remote_host = "",
-          .local_addr = "",
-          .method = "",
-          .password = "",
-          .remote_port = 0,
-          .local_port = 0,
-          .timeout = 0,
-          .acl = NULL,
-          .log = NULL,
-          .fast_open = 0,
-          .mode = 0,
-          .auth = 0,
-          .verbose = 0
-    };
+    profile_t * profile1 =  (profile_t *)profile;
+
+    printf("SSLocal: remote_host is %s:%d \n", profile1->remote_host, profile1->remote_port);
+    printf("SSLocal: local_addr is %s:%d \n", profile1->local_addr, profile1->local_port);
+    printf("SSLocal: method: %s, \n", profile1->method);
     
-    // Required
-    char * rh = malloc(sizeof(char)*(strlen(my_profile.remote_host)+1));
-    strcpy(rh, my_profile.remote_host);
-    profile1.remote_host = rh;
+    start_ss_local_server(*profile1);
     
-    char * la = malloc(sizeof(char)*(strlen(my_profile.local_addr)+1));
-    strcpy(la, my_profile.local_addr);
-    profile1.local_addr = la;
-    
-    char * mt = malloc(sizeof(char)*(strlen(my_profile.method)+1));
-    strcpy(mt, my_profile.method);
-    profile1.method = mt;
-    
-    char * pwd = malloc(sizeof(char)*(strlen(my_profile.password)+1));
-    strcpy(pwd, my_profile.password);
-    profile1.password = pwd;
-    
-    profile1.remote_port = my_profile.remote_port;
-    profile1.local_port = my_profile.local_port;
-    profile1.timeout = my_profile.timeout;
-    
-    char * ac = NULL;
-    if(my_profile.acl != NULL ){
-        ac = malloc(sizeof(char)*(strlen(my_profile.acl)+1));
-        strcpy(ac, my_profile.acl);
-        profile1.acl = ac;
-    }
-    
-    char * lg = NULL;
-    if(my_profile.log != NULL){
-        lg = malloc(sizeof(char)*(strlen(my_profile.log)+1));
-        strcpy(lg, my_profile.log);
-        profile1.log = lg;
-    }
-    
-    profile1.fast_open = my_profile.fast_open;
-    profile1.mode = my_profile.mode;
-    profile1.auth = my_profile.auth;
-    profile1.verbose = my_profile.verbose;
-    
-    printf("SSLocal: remote_host is %s:%d \n", profile1.remote_host, profile1.remote_port);
-    printf("SSLocal: local_addr is %s:%d \n", profile1.local_addr, profile1.local_port);
-    printf("SSLocal: method: %s, \n", profile1.method);
-    
-    start_ss_local_server(profile1);
-    
-    free(rh); free(la); free(mt); free(pwd);
-    if(ac != NULL){
-        free(ac);
-    }
-    if(lg != NULL){
-        free(lg);
-    }
+    // release memory
+    free(profile1->remote_host);
+    free(profile1->local_addr);
+    free(profile1->method);
+    free(profile1->password);
+    free(profile1->acl);
+    free(profile1->log);
+    free(profile1);
     
     isrunning = 0;
+    printf("SSLocal: Interrupted \n");
     pthread_mutex_unlock(&mutex);
     
-    printf("SSLocal: Interrupted\n");
-
     return 0;
 }
 
@@ -118,14 +62,54 @@ int sslocal_start(profile_t profile)
         isrunning = 1;
         pthread_mutex_unlock(&mutex);
         
-        my_profile = profile;
+        profile_t *cp_profile = malloc(sizeof(profile_t));
         
+        // Required
+        cp_profile->remote_host = malloc(sizeof(char)*(strlen(profile.remote_host)+1));
+        strcpy(cp_profile->remote_host, profile.remote_host);
+        
+        cp_profile->local_addr = malloc(sizeof(char)*(strlen(profile.local_addr)+1));
+        strcpy(cp_profile->local_addr, profile.local_addr);
+        
+        cp_profile->method = malloc(sizeof(char)*(strlen(profile.method)+1));
+        strcpy(cp_profile->method, profile.method);
+        
+        cp_profile->password = malloc(sizeof(char)*(strlen(profile.password)+1));
+        strcpy(cp_profile->password, profile.password);
+        
+        cp_profile->remote_port = profile.remote_port;
+        cp_profile->local_port = profile.local_port;
+        cp_profile->timeout = profile.timeout;
+        
+        // Optional
+        char * ac = NULL;
+        if(profile.acl != NULL ){
+            ac = malloc(sizeof(char)*(strlen(profile.acl)+1));
+            strcpy(ac, profile.acl);
+            cp_profile->acl = ac;
+        }else{
+            cp_profile->acl = NULL;
+        }
+        
+        char * lg = NULL;
+        if(profile.log != NULL){
+            lg = malloc(sizeof(char)*(strlen(profile.log)+1));
+            strcpy(lg, profile.log);
+            cp_profile->log = lg;
+        }else{
+            cp_profile->log = NULL;
+        }
+        
+        cp_profile->fast_open = profile.fast_open;
+        cp_profile->mode = profile.mode;
+        cp_profile->auth = profile.auth;
+        cp_profile->verbose = profile.verbose;
+
         printf("SSLocal: Start\n");
 
         ss_thread = NULL;
-        if(pthread_create( &ss_thread, NULL, do_ss_start, NULL)) {
+        if(pthread_create( &ss_thread, NULL, do_ss_start, cp_profile)) {
             printf("SSLocal: error creating thread\n");
-            
             return -1;
         }
         
@@ -145,7 +129,6 @@ int sslocal_stop()
 
         if( 0 == pthread_kill(*p, SIGUSR1) ){
             printf("SSLocal: Sent Signal to %p\n", ss_thread);
-        
             pthread_join(*p, NULL);
             printf("SSLocal: Stopped \n");
         }else{
